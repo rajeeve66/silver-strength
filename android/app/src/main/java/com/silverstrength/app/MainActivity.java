d154c427e55d55d5048ca8b8a66d1b4e9a7601c3
rajeev verma <rajeevverma@rajeevs-MacBook-Pro.local>
2026-04-19 09:52:36 +0400

    add capacitor android platform

---
+++ android/app/src/main/java/com/silverstrength/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.silverstrength.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.silverstrength.app',
+  appName: 'Silver-Strength',
+  webDir: 'dist'
+};
+
+export default config;
